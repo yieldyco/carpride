fd2bc72cdb060777d007bc79b0d014eb1b53b83f
Yevhen Vodianitskyi <veblife@gmail.com>
2026-01-22 13:46:42 +0200

    added storage/modification folder to git

---
+++ storage/modification/catalog/model/design/layout.php
@@ -0,0 +1,27 @@
+<?php
+class ModelDesignLayout extends Model {
+	public function getLayout($route) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "layout_route WHERE '" . $this->db->escape($route) . "' LIKE route AND store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY route DESC LIMIT 1");
+
+		if ($query->num_rows) {
+			return (int)$query->row['layout_id'];
+		} else {
+			return 0;
+		}
+	}
+	
+	public function getLayoutModules($layout_id, $position) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "layout_module WHERE layout_id = '" . (int)$layout_id . "' AND position = '" . $this->db->escape($position) . "' ORDER BY sort_order");
+		
+
+		// Code
+        if (isset($layout_id) && isset($position)) {
+       		if ($this->registry->get('seolanglib')) {
+       			$query->rows = $this->seolanglib->getLayoutModules($layout_id, $position, $query->rows);
+       		}
+        }
+		// End of code
+
+		return $query->rows;
+	}
+}
